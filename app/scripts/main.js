435ce617519df14e215e267930e4caba07ebd93e
Midge 't Hoen <midgethoen@gmail.com>
2017-02-14 09:18:37 +0100

    many work done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+function initMap() {
+  var home = {lat: 52.0182545, lng: 4.2067462};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: home,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    styles: [{
+      stylers: [{ saturation: -30 }]
+    }]
+  });
+  var marker = new google.maps.Marker({
+    position: home,
+    map: map
+  });
+}
